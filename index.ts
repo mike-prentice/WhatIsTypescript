2aaee02f48910e20cbe52f1230ce558eb9da4c6a
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-08 21:56:47 -0400

    compiling, nothing hooked up yet

--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
 // Import stylesheets
-import './style.css';
+let styles = require ("./style.css");
 
+const text = document.getElementById("defineword");
+const submit = document.getElementById("submit");
+// const form : HTMLFormElement = document.querySelector("#defineform");
+document.body.addEventListener("submit", async function (event) {
+  event.preventDefault();
+});
 
-const form: HTMLFormElement = document.querySelector('#defineform');
+// const result = await fetch(form.action, {
+//   method: form.method,
+//   body: new URLSearchParams([...new FormData(form) as any]),
+// })
+//   .then((response: Response)=> response.json())
+//   .then((json) => json)
+//   .catch((error) => console.log(error));
 
-
-form.onsubmit = () => {
-  const formData = new FormData(form);
-
-  console.log(formData);
-  const text = formData.get('defineword') as string;
+submit.onclick = () => {
   console.log(text);
+  // const formData = new FormData(form);
+  // console.log(formData);
+  // const text = formData.get("defineword") as string;
+  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + text)
+    .then((response: Response) => response.json())
+    .then((json) => json)
+    .catch((error) => console.log(error));
   return false; // prevent reload
 };
