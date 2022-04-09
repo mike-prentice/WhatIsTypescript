843d75c691eea11408cc4a0daac07e88d5290d42
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-08 23:45:09 -0400

    fetch working and returning input

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,23 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const form: HTMLFormElement = document.querySelector('#defineform');
+  document.body.addEventListener("submit", async function(event){
+  event.preventDefault();
+   let form = event.target as HTMLFormElement;
+      });
+
+form.onsubmit = () => {
+  const formData = new FormData(form);
+  console.log(formData);
+  const text = formData.get('defineword') as string;
+  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + text)
+      .then((response: Response) => response.json())
+      .then(function(data){console.log(data)})
+      .catch((error) => console.log(error));
+  console.log(text);
+  return true; // prevent reload
+
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          {/* <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1>Typescript Dictionary</h1>
+          </header> */}
+      </div>
   );
 }
 
