5883cf3c133b86c3b543ee573dbc575ee5c8f447
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-10 16:07:29 -0400

    some ui cleanup

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -9,22 +9,21 @@ function App() {
   
   const [def, setDef] = useState([]);
   
-
-
+  
   const form: HTMLFormElement = document.querySelector("#defineform");
   document.body.addEventListener("submit", async function (event) {
     event.preventDefault();
     let form = event.target as HTMLFormElement;
   });
 
-  
+ 
   
     useEffect(() => {
       const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
       form.onsubmit = async () => {
         const formData = new FormData(form);
         // console.log(formData);
-        const text = formData.get("defineword") as string;
+         const text = formData.get("defineword") as string;
         axios.get(url + text)
           .then((res) => {
             console.log(res);
@@ -54,36 +53,32 @@ function App() {
 
     return (
       <div className="App">
-    
          <main className="container">
-          
           <div className="bg-light p-5 rounded">
-            <h1 className="defined">Definition</h1>
-            <p className="lead">
-           
-            </p>
+            <h1 className="defined">Dictionary App</h1>
+            <p className="lead">******************************************</p>
             {def.map((def) => (
-            <ul className="list-unstyled">
-              <li>{def.word}</li>
-                <li>Phonetic: {def.phonetic}</li>
-              <li></li>
-                <li>Definition: {def.meanings[0].definitions[0].definition}</li>
-              <li>
-                However, this style only applies to immediate child elements.
-              </li>
-              <li>
-                Synonyms and Antonyms
-                <ul>
-                  <li>Synonyms: {def.meanings[0].synonyms}</li>
-                  <li>Antonyms: {def.meanings[0].antonyms}</li>
-                  <li>and have appropriate left margin</li>
-                </ul>
-              </li>
-              <li>This may still come in handy in some situations.</li>
+              <ul className="list-unstyled">
+                <li>{def.word}</li>
+                <li><i>Phonetic:</i> {def.phonetic}</li>
+                <li><i>Part of Speech:</i>{def.meanings[0].partOfSpeech}</li>                
+                <li><i>Synonyms:</i> {def.meanings[0].synonyms}</li>
+                <li><i>Antonyms:</i> {def.meanings[0].antonyms}</li>
+                <li><i>Origin:</i> {def.origin}</li>
+                {def.meanings.map((meanings) => (
+                  <ul className="list-unstyled">
+                      <li>{meanings.partsOfSpeech}</li>
+                    {meanings.definitions.map((definitions) => (
+                      <ul className = "list-unstyled">
+                        <li><i>Definition:</i> {definitions.definition}</li>
+                        <li>*************************************************</li>
+                      </ul>
+                    ))}
+                  </ul>
+                ))}
               </ul>
               ))}
-           </div>
-           
+          </div>
         </main>
       </div> 
     );
